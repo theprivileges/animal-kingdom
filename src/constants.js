7f059770187bc91d846a880c65077130d7ee78cf
Luiz Lopes <luiz@wickeddeveloper.com>
2019-07-17 09:11:27 -0600

    Add moxiegirl's kingdom

--- src/constants.js
+++ src/constants.js
@@ -63,5 +63,10 @@ export const OTHER_KINGDOMS = [
   {
       app: 'https://thirsty-jang-0c0a17.netlify.com',
       ruler: 'ma1222042.id.blockstack'
+  },
+
+  {
+      app: 'https://animalkingdoms.netlify.com',
+      ruler: 'moxiegirl.id.blockstack'
   }
 ]
